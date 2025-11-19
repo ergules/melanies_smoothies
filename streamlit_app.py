172a42654b2e4ca4df1e97af494016b126081bf1
Muhammet Ergül <muhammet.e@inxpress.com>
2025-11-19 16:31:00 +0300

    introduce api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
